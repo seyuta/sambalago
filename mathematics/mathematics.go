81ead1a421ae22915144e8b5b8b7115d719788b2
seyuta <seyuta.a.h@gmail.com>
2025-04-21 12:48:00 +0700

    feat: mean big float & standard deviation big float

--- mathematics/mathematics.go
+++ mathematics/mathematics.go
@@ -1,6 +1,9 @@
 package mathematics
 
-import "math"
+import (
+	"math"
+	"math/big"
+)
 
 // Function to calculate the mean / average with variadic parameter
 func Mean[T float32 | float64](elements ...T) float64 {
@@ -23,4 +26,43 @@ func StandardDeviation[T float32 | float64](elements ...T) float64 {
 	variance := varianceSum / float64(len(elements)-1)
 
 	return math.Sqrt(variance)
+}
+
+// Calculates the mean (average) of variadic *big.Float values
+func MeanBigFloat(precisionBits uint, elements ...*big.Float) *big.Float {
+	sum := new(big.Float).SetPrec(precisionBits).SetFloat64(0)
+
+	for _, e := range elements {
+		sum.Add(sum, e)
+	}
+
+	count := new(big.Float).SetPrec(precisionBits).SetInt64(int64(len(elements)))
+	mean := new(big.Float).SetPrec(precisionBits).Quo(sum, count)
+
+	return mean
+}
+
+// Calculates the standard deviation of variadic *big.Float values
+func StandardDeviationBigFloat(precisionBits uint, elements ...*big.Float) *big.Float {
+	if len(elements) <= 1 {
+		// Return 0 if there's only one or no element to avoid division by zero
+		return big.NewFloat(0).SetPrec(precisionBits)
+	}
+
+	mean := MeanBigFloat(precisionBits, elements...)
+	varianceSum := new(big.Float).SetPrec(precisionBits).SetFloat64(0)
+
+	for _, e := range elements {
+		diff := new(big.Float).SetPrec(precisionBits).Sub(e, mean)
+		squared := new(big.Float).SetPrec(precisionBits).Mul(diff, diff)
+		varianceSum.Add(varianceSum, squared)
+	}
+
+	// Divide by (n - 1) for sample standard deviation
+	nMinusOne := new(big.Float).SetPrec(precisionBits).SetInt64(int64(len(elements) - 1))
+	variance := new(big.Float).SetPrec(precisionBits).Quo(varianceSum, nMinusOne)
+
+	// Return the square root of the variance
+	stdDev := new(big.Float).SetPrec(precisionBits).Sqrt(variance)
+	return stdDev
 }
